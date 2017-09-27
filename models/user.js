37f3295046e39d06d33d3beb8e05196a89da59b7
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-09-27 10:59:15 -0600

    hashing password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -46,6 +46,18 @@ const userSchema = new Schema({
     type: Schema.ObjectId,
     ref: 'User'
   }]
+})
+
+// hash password before saving to database
+userSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
 })
 
 const model = mongoose.model('User', userSchema)
