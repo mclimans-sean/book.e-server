f9d51339074c4b78b069a9570bfd2f9d5e5f5562
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-09-27 11:06:44 -0600

    comparing hashed password

--- routes/users.js
+++ routes/users.js
@@ -31,8 +31,12 @@ router.post('/login', (req, res, next) => {
       console.log(err);
       res.status(400).json(err)
     } else {
-      console.log('User found', user);
-      res.status(200).json(user)
+      if (bcrypt.compare(password, user.password)) {
+        console.log('User found', user);
+        res.status(200).json(user)
+      } else {
+        res.status(401).json('Unauthorized');
+      }
     }
   })
 })
