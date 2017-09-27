20c6a8bcbb3d782416da0048764847518d456f04
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-09-27 11:15:22 -0600

    login routes

--- models/user.js
+++ models/user.js
@@ -48,6 +48,27 @@ const userSchema = new Schema({
   }]
 })
 
+// authenticate input against database documents
+userSchema.statics.authenticate = function (email, password, callback) {
+  User.findOne({email: email})
+    .exec(function (error, user) {
+      if (error) {
+        return callback(error);
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function (error, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    })
+}
+
 // hash password before saving to database
 userSchema.pre('save', function (next) {
   var user = this;
--- routes/users.js
+++ routes/users.js
@@ -31,12 +31,8 @@ router.post('/login', (req, res, next) => {
       console.log(err);
       res.status(400).json(err)
     } else {
-      if (bcrypt.compare(password, user.password)) {
-        console.log('User found', user);
-        res.status(200).json(user)
-      } else {
-        res.status(401).json('Unauthorized');
-      }
+      console.log('User found', user);
+      res.status(200).json(user)
     }
   })
 })
